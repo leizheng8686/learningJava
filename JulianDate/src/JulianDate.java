1cf375e9779105ba5f43eaa16eaf966960853e21
86 <leizheng8686@gmail.com>
2016-03-13 17:07:51 -0400

    noHourMinSec

--- JulianDate/src/JulianDate.java
+++ JulianDate/src/JulianDate.java
@@ -18,27 +18,62 @@
  */
 public class JulianDate { 
 	private double julianDate;
-	private int year,month,day;
+	private int year,month,day,hour,min,sec;
 	// only one variable monthNames or ALL JulianDate objects (shared)
 	private final static String[] monthNames = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 	public JulianDate(int year, int mon, int day) {
+//		year--;
 		if(mon<3) { year--; mon+=12; }
+//		System.out.println(mon);
 		julianDate = (year*365) +(year/4) -(year/100) +(year/400)
 	              +(mon*153+3)/5-92
-	              +day-1
-		;
+	              +day-1;
+//	    System.out.println(((year*365) +(year/4) -(year/100) +(year/400))+" "+((mon*153+3)/5-92)+" "+julianDate);
+	              
 	}
 	public JulianDate(int year, int mon, int day,
 				int hour, int min, int sec) {
-			
+			this.sec = sec%60;
+			this.min = (sec/60 + min)%60;
+			this.hour = ((sec/60 + min)/60 + hour)%24;
+			this.day = ((sec/60 + min)/60 + hour)/24 + day;
+			if(mon<3) { year--; mon+=12; }
+			julianDate = (year*365) +(year/4) -(year/100) +(year/400)
+		              +(mon*153+3)/5-92
+		            +day-1;
+	}
+	public JulianDate(double jd){
+		julianDate = jd;
 	}
 	public void updateYMD(){
 		int j = (int)julianDate; 
-		year = j/(146097)*400+
-				(j-j/(146097)*146097)/36524*100+
-				  ((j-j/(146097)*146097)-(j-j/(146097)*146097)/36524*36524)/1461*4+
-				    (((j-j/(146097)*146097)-(j-j/(146097)*146097)/36524*36524)-((j-j/(146097)*146097)-(j-j/(146097)*146097)/36524*36524)/1461*1461)/365;
-		month = j - ((year*365) +(year/4) -(year/100) +(year/400));
+		year = (int)(julianDate/365.25);
+//		year = j/(146097)*400+
+//				(j-j/(146097)*146097)/36524*100+
+//				  ((j-j/(146097)*146097)-(j-j/(146097)*146097)/36524*36524)/1461*4+
+//				    (((j-j/(146097)*146097)-(j-j/(146097)*146097)/36524*36524)-((j-j/(146097)*146097)-(j-j/(146097)*146097)/36524*36524)/1461*1461)/365;
+//		System.out.println(j+" "+((year*365) +(year/4) -(year/100) +(year/400)));
+		month = (int)((julianDate - ((year*365) +(year/4) -(year/100) +(year/400)))/30.6);
+		month += 3;
+//		System.out.println(month+" "+((month*153+3)/5-92));
+		day = (int)(julianDate -((year*365) +(year/4) -(year/100) +(year/400)
+	              +(month*153+3)/5-92)+1);
+		if(month>12){
+			month -= 12;
+			year++;
+		}
+		if(month == 2 && day>28){
+			if((year%400 == 0) || ((year%100 != 0) && (year%4 == 0))){
+				if(day > 29){
+					month++;
+					day -= 29;
+				}
+			}
+			else{
+				month++;
+				day -= 28;
+			}
+		}
 	}
 	public int getYear() {
 		updateYMD();
@@ -53,14 +88,25 @@ public class JulianDate {
 		return day;
 	}
 //	// return 0 for Sunday, 1=monday, ... 6 = Saturday
-//	public int dayOfWeek() {
-//		
-//	}
-//	public String getMonthName() {
-//		
-//	}
+	public String dayOfWeek() {
+		String weekday[] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+		return weekday[(int)((julianDate+2)%7)];
+	}
+	public String getMonthName() {
+		updateYMD();
+		return monthNames[month-1];
+	}
+	public JulianDate add(double d){
+		return new JulianDate(julianDate+d);
+	}
+	public JulianDate sub(double d){
+		return new JulianDate(julianDate-d);
+	}
+	public double sub(JulianDate jd){
+		return this.julianDate - jd.julianDate;
+	}
 	public String toString(){
-		return getMonth()+"/"+getDay()+"/"+getYear();
+		return getYear()+"."+getMonthName()+"."+getDay();
 	}
 	
 	
@@ -69,14 +115,14 @@ public class JulianDate {
 		JulianDate d1 = new JulianDate(2016, 2, 29);
 		JulianDate d2 = new JulianDate(2016, 3, 1);
 		System.out.println(d1);
-//		JulianDate d3 = d1.add(1); // tomorrow
-//		System.out.println(d3);  // 2016 Mar. 1
-//		JulianDate d4 = d1.add(-1); //yesterday
-//		JulianDate d5 = d1.add(1.5); //tomorrow noon
-//		JulianDate d6 = d5.sub(2); // 2 days before d2
-//		double days = d5.sub(d2);
-//		System.out.println(d1);  // 2016 Feb. 29
-//		JulianDate d7 = new JulianDate(2017, 2, 27);
-//		System.out.println(d7.add(2)); // 2017 Mar. 1
+		JulianDate d3 = d1.add(1); // tomorrow
+		System.out.println(d3);  // 2016 Mar. 1
+		JulianDate d4 = d1.add(-1); //yesterday
+		JulianDate d5 = d1.add(1.5); //tomorrow noon
+		JulianDate d6 = d5.sub(2); // 2 days before d2
+		double days = d5.sub(d2);
+		System.out.println(d1);  // 2016 Feb. 29
+		JulianDate d7 = new JulianDate(2017, 2, 27);
+		System.out.println(d7.add(2)); // 2017 Mar. 1
 	}
 }
